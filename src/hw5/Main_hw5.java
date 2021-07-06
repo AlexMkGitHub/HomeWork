f899ea6d66803df5fd7a725fc03cead3ccaafd4d
Aleksandr <mkalex77@gmail.com>
2021-07-07 03:53:16 +0700

    Домашняя работа 5 (коррекция)

--- src/hw5/Main_hw5.java
+++ src/hw5/Main_hw5.java
@@ -4,16 +4,36 @@ public class Main_hw5 {
 
     public static void main(String[] args) {
 
-        Person person1 = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com",
-                "892312312", 30000, 30);
-        Person person2 = new Person("Petrov Ilya", "Engineer", "petrov@mailbox.com",
-                "892357890", 35000, 40);
-        Person person3 = new Person("Sidorov Evgeniy", "Tester", "sidorov@mailbox.com",
-                "892342155", 27000, 28);
-        Person person4 = new Person("Koshkin Andrey", "Administration", "koshkin@mailbox.com",
-                "892390101", 70000, 42);
-        Person person5 = new Person("Volin Nikita", "Driver", "driverman@mailbox.com",
-                "892350034", 38000, 25);
+        Person person1 = new Person("Ivanov Ivan",
+                "Engineer",
+                "ivivan@mailbox.com",
+                "892312312",
+                30000,
+                30);
+        Person person2 = new Person("Petrov Ilya",
+                "Engineer",
+                "petrov@mailbox.com",
+                "892357890",
+                35000,
+                40);
+        Person person3 = new Person("Sidorov Evgeniy",
+                "Tester",
+                "sidorov@mailbox.com",
+                "892342155",
+                27000,
+                28);
+        Person person4 = new Person("Koshkin Andrey",
+                "Administration",
+                "koshkin@mailbox.com",
+                "892390101",
+                70000,
+                42);
+        Person person5 = new Person("Volin Nikita",
+                "Driver",
+                "driverman@mailbox.com",
+                "892350034",
+                38000,
+                25);
 
 //Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
         System.out.println("Задание 3:");
@@ -21,16 +41,36 @@ public class Main_hw5 {
         System.out.println();
 
         Person[] person = new Person[5];
-        person[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312",
-                30000, 30);
-        person[1] = new Person("Petrov Ilya", "Engineer", "petrov@mailbox.com",
-                "892357890", 35000, 41);
-        person[2] = new Person("Sidorov Evgeniy", "Tester", "sidorov@mailbox.com",
-                "892342155", 27000, 28);
-        person[3] = new Person("Koshkin Andrey", "Administration", "koshkin@mailbox.com",
-                "892390101", 70000, 42);
-        person[4] = new Person("Volin Nikita", "Driver", "driverman@mailbox.com",
-                "892350034", 38000, 25);
+        person[0] = new Person("Ivanov Ivan",
+                "Engineer",
+                "ivivan@mailbox.com",
+                "892312312",
+                30000,
+                30);
+        person[1] = new Person("Petrov Ilya",
+                "Engineer",
+                "petrov@mailbox.com",
+                "892357890",
+                35000,
+                41);
+        person[2] = new Person("Sidorov Evgeniy",
+                "Tester",
+                "sidorov@mailbox.com",
+                "892342155",
+                27000,
+                28);
+        person[3] = new Person("Koshkin Andrey",
+                "Administration",
+                "koshkin@mailbox.com",
+                "892390101",
+                70000,
+                42);
+        person[4] = new Person("Volin Nikita",
+                "Driver",
+                "driverman@mailbox.com",
+                "892350034",
+                38000,
+                25);
 
 
         System.out.println("Задание 5:");
