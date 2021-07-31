8aa33c4c633822423d8b63c04cf3e0fe08c831f2
Aleksandr <mkalex77@gmail.com>
2021-08-01 06:04:32 +0700

    Домашняя работа 4 (курс Java 2)

---
+++ Java2/hw4/sample/Controller.java
@@ -0,0 +1,21 @@
+package hw4.sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    public TextField textField;
+
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    public void send(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+}
