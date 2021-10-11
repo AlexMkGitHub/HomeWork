6969c7a5920aad0c82b78905eb81db4491b0e3b3
AlexMkGitHub <mkalex77@gmail.com>
2021-10-12 06:27:35 +0700

    Домашняя работа 1 (Java 3)

---
+++ Java3/src/com/company/Apple.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Apple extends Fruit {
+    private float getWEIGHT() {
+        return WEIGHT;
+    }
+    final float WEIGHT = 1.0f;
+
+}
---
+++ Java3/src/com/company/Orange.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Orange extends Fruit {
+    final float WEIGHT = 1.5f;
+
+    private float getWEIGHT() {
+        return WEIGHT;
+    }
+
+}
